82bd287a94e51d106252ff4bcf2e5f6d2ef69f50
AvengerEug <eugenesumarry@163.com>
2020-02-22 18:33:54 +0800

    新增eureka集群module以及cap概念

--- customize-wfw/eureka8002/src/main/java/com/eugene/sumarry/customize/wfw/Eureka8002Application.java
+++ customize-wfw/eureka8002/src/main/java/com/eugene/sumarry/customize/wfw/Eureka8002Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class Eureka8002Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(Eureka8002Application.class, args);
     }
 }
